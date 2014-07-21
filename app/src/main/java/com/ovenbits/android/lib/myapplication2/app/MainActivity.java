3304fa9bbb197d2e167b915c494d90eb2f415ea9
Ehsan Barekati <ehsan1772@yahoo.com>
2014-07-21 17:02:26 -0500

    testing travis after enabling

--- app/src/main/java/com/ovenbits/android/lib/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/ovenbits/android/lib/myapplication2/app/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //test for travis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
